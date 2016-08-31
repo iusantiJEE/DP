b2058d48d74b97fe107be95e6f575115f3738999
Iustin Antimoianu <iustin.antimoianu@fashiondays.com>
2016-08-31 10:59:01 +0300

    update

--- DP_AbstractFactory3/src/DP_AF3.java
+++ DP_AbstractFactory3/src/DP_AF3.java
@@ -1,16 +1,41 @@
 import abstracts.FactoryProducer;
 import abstracts.GUIFactory;
+import abstracts.MyScrollbar;
+import abstracts.MyWindow;
 
 public class DP_AF3 {
 	public static void main(String[] data){
-		FactoryProducer factory =  new FactoryProducer();
+//		FactoryProducer factory =  new FactoryProducer();
+//		
+//		GUIFactory guiFactory = factory.getGUIFactory(FactoryProducer.FactoryType.OSX_FACTORY);
+//		guiFactory.createMyScrollBar().paint();
+//		guiFactory.createMyWindow().paint();
+//		
+//		guiFactory = factory.getGUIFactory(FactoryProducer.FactoryType.WIN_FACTORY);
+//		guiFactory.createMyScrollBar().paint();
+//		guiFactory.createMyWindow().paint();
 		
-		GUIFactory guiFactory = factory.getGUIFactory(FactoryProducer.FactoryType.OSX_FACTORY);
-		guiFactory.createMyScrollBar().paint();
-		guiFactory.createMyWindow().paint();
+//		OR
 		
-		guiFactory = factory.getGUIFactory(FactoryProducer.FactoryType.WIN_FACTORY);
-		guiFactory.createMyScrollBar().paint();
-		guiFactory.createMyWindow().paint();
+		FactoryProducer factoryProducer =  new FactoryProducer();
+		GUIFactory guiFactory =  null;
+		
+		guiFactory =  factoryProducer.getGUIFactory(FactoryProducer.FactoryType.OSX_FACTORY);
+		
+		MyScrollbar scrollbar = guiFactory.createMyScrollBar();
+		scrollbar.paint();
+		
+		MyWindow window = guiFactory.createMyWindow();
+		window.paint();
+		
+		//---
+		guiFactory = factoryProducer.getGUIFactory(FactoryProducer.FactoryType.WIN_FACTORY);
+		
+		scrollbar = guiFactory.createMyScrollBar();
+		scrollbar.paint();
+		
+		window = guiFactory.createMyWindow();
+		window.paint();
+		
 	}
 }
